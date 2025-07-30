94469852b98716533a60e96fa7e5b73439c2e3ce
RogerBarbo <rogerb.barbo@gmail.com>
2025-07-29 21:27:42 -0300

    Adicionar arquivos de projeto.

---
+++ ApiEntra21/Controllers/AlunoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Modelo.Domain;
+
+namespace ApiEntra21.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : Controller
+    {
+        [HttpGet("BuscarDadosAluno/{id}")]
+        public IActionResult BuscarDadosAluno(int id)
+        {
+            try
+            {
+                Aluno aluno = new Aluno();
+                aluno.Id = 1;
+                aluno.Nome = "Roger Barbosa de Oliveira";
+                aluno.Idade = 17;
+                aluno.Cep = "12345-678";
+
+                return Ok(aluno);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+    }
+}
